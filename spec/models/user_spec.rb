c97b89bc3dc63bb276389235a474b2f2b6301d97
Tetsuya Azami <zqianjian35@gmail.com>
2021-09-30 11:13:09 +0900

    add rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-it 'テストが成功すること' do
-  expect("hogehoge").to eq "hogehoge"
-end
+  it 'テストが成功すること' do
+    expect('hogehoge').to eq 'hogehoge'
+  end
 end
